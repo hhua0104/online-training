606cac8dd4c4643076570da2c7a57d8bb0e4e645
hhua0104 <hhua0104@outlook.com>
2018-07-04 13:46:09 +0800

    111

---
+++ application/logs/log-2018-07-04.php
@@ -0,0 +1,91 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-07-04 07:11:51 --> Severity: Warning --> Division by zero D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\views\dashboard.php 231
+ERROR - 2018-07-04 07:12:28 --> Severity: Warning --> Division by zero D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\views\dashboard.php 231
+ERROR - 2018-07-04 08:48:39 --> Severity: Warning --> Division by zero D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\views\dashboard.php 231
+ERROR - 2018-07-04 09:40:15 --> Severity: Warning --> Division by zero D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\views\dashboard.php 231
+ERROR - 2018-07-04 09:45:43 --> Could not find the language line "hello"
+ERROR - 2018-07-04 09:45:43 --> Could not find the language line "user_id"
+ERROR - 2018-07-04 09:45:43 --> Could not find the language line "notattempted"
+ERROR - 2018-07-04 10:36:54 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:19 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:27 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:29 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:31 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:34 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:38 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:40 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:42 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:44 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:46 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:48 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:50 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:52 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:54 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:56 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:37:59 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:01 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:03 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:05 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:07 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:09 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:11 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:13 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:15 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:17 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:19 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:21 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:24 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:26 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:28 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:30 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:32 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:34 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:36 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:38 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:40 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:42 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:44 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:46 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:49 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:51 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:53 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:55 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:57 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:38:59 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:01 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:03 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:05 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:07 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:09 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:12 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:14 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:16 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:18 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:20 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:22 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:24 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:26 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:28 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:30 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:32 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:34 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:36 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:39 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:41 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:43 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:45 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:47 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:49 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:51 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:56 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:39:58 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:40:00 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:40:02 --> Severity: Warning --> Invalid argument supplied for foreach() D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\models\Quiz_model.php 729
+ERROR - 2018-07-04 10:40:09 --> Could not find the language line "hello"
+ERROR - 2018-07-04 10:40:09 --> Could not find the language line "user_id"
+ERROR - 2018-07-04 10:40:09 --> Could not find the language line "notattempted"
+ERROR - 2018-07-04 10:49:27 --> Severity: Warning --> Division by zero D:\phpStudy\PHPTutorial\WWW\savsoftquiz_v4.0_advance\application\views\dashboard.php 231
+ERROR - 2018-07-04 11:07:03 --> Could not find the language line "hello"
+ERROR - 2018-07-04 11:07:03 --> Could not find the language line "user_id"
+ERROR - 2018-07-04 11:07:03 --> Could not find the language line "notattempted"
